4097e7ba9be1353e9fb438a7348c250ec9da8081
Andrés Romaña <xandrewssx970@outlook.es>
2026-04-08 19:17:22 -0500

    Add routing configuration for client list and detail views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ListaClientesComponent } from './components/clientes/lista-clientes/lista-clientes.component';
+import { DetalleClienteComponent } from './components/clientes/detalle-cliente/detalle-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'clientes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'clientes',
+    component: ListaClientesComponent,
+  },
+  {
+    path: 'cliente/:id',
+    component: DetalleClienteComponent,
+  },
+];
